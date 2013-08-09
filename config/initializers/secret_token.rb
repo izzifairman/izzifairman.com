c91281e508960aa3ea5e03649c17fc1023a65051
izzifairman <hulk_rules64@yahoo.com>
2013-08-09 13:15:12 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Izzifairman::Application.config.secret_key_base = '66908a302c5af266c17f540839bd6a003745c94ef1ef97d8ba7ec3fb4fe52dcc4b5cd4cbf05c4f710bb68af46479d26fbced687ab4067955c8d9f66fbdab5f3e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Izzifairman::Application.config.session_store :cookie_store, key: '_izzifairman_session'
